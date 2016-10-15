64c709346b57f9c71b0b126ef3660df8a1ae2b43
Sam Beckmann <samvbeckmann@gmail.com>
2016-10-15 17:08:26 -0500

    Made cool colors and transitions

---
+++ js/colorStates.js
@@ -0,0 +1,22 @@
+var map = d3.select(".map");
+var states = map.selectAll('path');
+
+var color = d3.scale.category20();
+
+states.attr('fill', function(d, i) {
+  return color(i);
+});
+
+states.on("mouseover", function(d) {
+  var stateUnderMouse = this;
+  d3.selectAll('path').transition().style('opacity', function() {
+    return (this === stateUnderMouse) ? 1.0 : 0.5;
+  });
+});
+
+states.on("mouseout", function(d) {
+  d3.selectAll('path').transition().style('opacity', function() {
+    return 1.0;
+  });
+});
+ß
