49fa3691ca48e65678eb001b3ed9e935329a6ba1
Sam Beckmann <samvbeckmann@gmail.com>
2016-10-15 23:59:08 -0500

    Added fancy effects

--- js/colorStates.js
+++ js/colorStates.js
@@ -8,16 +8,24 @@ var xCircleOffset = d3.scaleLinear()
   .domain([0, 50])
   .range([10, 890]);
 
+var selected = null;
+
 d3.json("data/states.json", function(error, data) {
 
   var color = d3.scaleLinear()
-    .domain([0, d3.max(data.states, function(d) {
-      return d.trump;
-    })])
-    .range(["white", "green"]);
+    .domain([0, 1])
+    .range(["#e7f2e7", "green"]);
+
+  function colorState(d, hover) {
+    if (d.name === selected) {
+      return hover ? d3.rgb(255, 236, 180) : d3.rgb(255, 198, 71);
+    } else {
+      return color(d.score);
+    }
+  }
 
   states.attr('fill', function(d, i) {
-    return color(data.states[i].trump);
+    return color(data.states[i].score);
   });
 
   var circle = circles.selectAll("circle").data(data.states);
@@ -25,25 +33,36 @@ d3.json("data/states.json", function(error, data) {
   circle.enter().append("circle")
       .attr("r", 8)
       .attr("cy", 10)
-      .attr("cx", function(d, i) { return xCircleOffset(i); })
-      .style("fill", "black");
+      .attr("cx", function(d, i) { return xCircleOffset(d.rank); })
+      .style("fill", color(.3));
 
-});
-
-states.on("mouseover", function() {
-  var stateUnderMouse = this;
-  d3.selectAll('path').transition().style('opacity', function() {
-    return (this === stateUnderMouse) ? 1.0 : 0.5;
+  states.on("mouseover", function(d, i) {
+    var stateUnderMouse = this;
+    d3.selectAll('path').transition().style('fill', function(d, i) {
+      return (this === stateUnderMouse) ? d3.rgb(255, 198, 71) : colorState(data.states[i], true);
+    });
+    d3.selectAll('circle').transition().style('fill', function(d, i) {
+      return (stateUnderMouse.rank === i) ? d3.rgb(255, 198, 71) : color(.3);
+    });
+    d3.select(".state-name").text(data.states[i].name);
   });
-});
 
-states.on("mouseout", function() {
-  states.transition().style('opacity', function() {
-    return 1.0;
+  states.on("mouseout", function() {
+    states.transition().style('fill', function(d, i) {
+      return colorState(data.states[i], false);
+    });
+    if (selected == null) {
+      d3.select(".state-name")
+          .text("Select A State")
+          .style("color", "grey");
+    } else {
+      d3.select(".state-name")
+          .text(selected)
+          .style("color", "black");
+    }
   });
-});
 
-states.on("click", function(d) {
-  var id = d3.select(this).attr("id");
-  d3.select(".state-name").text(id);
+  states.on("click", function(d, i) {
+    selected = data.states[i].name;
+  });
 });
